4d1f61aaba27454af7840c65ffe494564e962625
Felipe Tofoli <fatofoli@servpronet.com>
2022-02-16 00:14:05 -0300

    Added axios and requests to GitHub api

--- src/Github.js
+++ src/Github.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Github() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState({});
+
+  const api = axios.create({
+    baseURL: "https://api.github.com/users/",
+  });
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    if (username) {
+      api
+        .get(username)
+        .then((response) => {
+          const responseUser = {
+            username: response.data.login,
+            name: response.data.name,
+            avatar: response.data.avatar_url,
+            publicReposQuantity: response.data.public_repos,
+          };
+          setUser(responseUser);
+        })
+        .catch((err) => {
+          setUser({});
+          if (err.response.status === 404) alert("Username not found");
+          else alert("error");
+        });
+    } else {
+      setUser({});
+      alert("Please fill the username field");
+    }
+  };
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
   return (
     <>
       <h1>Github Profile</h1>
-      <input type="text" placeholder="Type the user name" />
-      <button type="submit">Search</button>
+      <form>
+        <input
+          type="text"
+          placeholder="Type the user name"
+          value={username}
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={handleClick}>
+          Search
+        </button>
+      </form>
+
+      <div
+        style={{ display: Object.keys(user).length === 0 ? "none" : "block" }}
+      >
+        <h2>{user.username}</h2>
+        <img src={user.avatar} alt={user.name} />
+        <p>Name: {user.name}</p>
+        <p>Public repos: {user.publicReposQuantity}</p>
+      </div>
     </>
   );
 }
