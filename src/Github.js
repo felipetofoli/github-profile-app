e39af2c537ff81ba1efed1412eff22a9c183f421
Felipe Tofoli <fatofoli@servpronet.com>
2022-02-15 23:22:59 -0300

    Added Github component with initial layout

---
+++ src/Github.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Github() {
+  return (
+    <>
+      <h1>Github Profile</h1>
+      <input type="text" placeholder="Type the user name" />
+      <button type="submit">Search</button>
+    </>
+  );
+}
+
+export default Github;
