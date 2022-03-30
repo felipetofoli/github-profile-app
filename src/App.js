3a1f2878f083dd5ed134607b4f09cf788f1928e0
Felipe Tofoli <fatofoli@servpronet.com>
2022-03-29 23:38:35 -0300

    Added routes to constant

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserProfileListComponent } from "./components";
 
 function App() {
+  const routes = [
+    {
+      path: "/",
+      element: (
+        <div className="App">
+          <Github />
+        </div>
+      ),
+    },
+    {
+      path: "/profile/:username",
+      element: <UserProfileListComponent />,
+    },
+  ];
+
   return (
     <Provider store={Store}>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="App">
-                <Github />
-              </div>
-            }
-          />
-          <Route
-            path="/profile/:username"
-            element={<UserProfileListComponent />}
-          />
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </BrowserRouter>
     </Provider>
