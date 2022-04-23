d9f0ef1c44654a5bf0121db556fe25de5f4d882f
Felipe Tofoli <fatofoli@servpronet.com>
2022-04-23 19:39:04 -0300

    Added basic tests checking initial screen.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("GithubProfile", () => {
+  it("should render the header", () => {
+    render(<App />);
+    const headerElement = screen.getByText(/HEADER/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it("should render the Github profile", () => {
+    render(<App />);
+    const gitHubProfileElement = screen.getByRole("heading", {
+      name: "Github Profile",
+    });
+    expect(gitHubProfileElement).toBeInTheDocument();
+  });
+
+  it("should render the footer", () => {
+    render(<App />);
+    const footerElement = screen.getByText(/FOOTER/);
+    expect(footerElement).toBeInTheDocument();
+  });
 });
